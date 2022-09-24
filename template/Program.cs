11f7eeaea6a03fdcac10ec5dedfc60fdfaea0aaa
Douglas Urner <dlu@canishe.com>
2022-09-23 22:04:31 -0700

    Beginning of template project
    Starting with command line parsing.

---
+++ template/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Template
+{
+  using System;
+
+  using FluentArgs;
+
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      FluentArgsBuilder.New()
+        .DefaultConfigs()
+        .Parameter<uint>("--start-on")
+          .IsOptionalWithDefault(7)
+        .Parameter("--student-list").IsRequired()
+        .Flag("--verbose", "-v")
+        .Call(verbose => studentList => startOn =>
+          {
+            if (verbose) Console.WriteLine($"Skipping first {startOn - 1} records");
+            //Process(studentList);
+            Console.WriteLine($"Process {studentList}");
+          }
+        )
+        .Parse(args);        
+    }
+    void Process(string studentList)
+    {
+      Console.WriteLine($"Process {studentList}");
+    }
+  }
+}
